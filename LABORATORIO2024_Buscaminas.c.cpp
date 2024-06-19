0ed1cfe17eab2116e6ee221b908a264d7c3e6c3f
Lia Hannay Dietrich <liahannay@hotmail.com>
2024-06-18 21:26:48 -0300

    Update LABORATORIO2024_Buscaminas.c.cpp

--- LABORATORIO2024_Buscaminas.c.cpp
+++ LABORATORIO2024_Buscaminas.c.cpp
@@ -525,11 +525,9 @@ void buscar(int fila, int columna) {
 }
 
 
-
 void rendirse() {
-	printf("Usted se ha rendido.\n");
 	revelar_todas_las_bombas();
-	game_over = 1;
+	game_over = 2;
 }
 
 void volver_al_menu_principal() {
@@ -596,16 +594,17 @@ void jugar() {
 	} else if (game_over) {
 		printf("\n¡¡¡¡ PERDISTE !!!! Suerte la próxima, se nota que la necesitas.\n");
 	}
-	
-	// Almacenar la partida
+	else if (game_over == 2) {
+		printf ("\n Has abandonado la partida.\n");
+	}// Almacenar la partida
 	time_t t = time(NULL);
 	struct tm tm = *localtime(&t);
 	char fecha_partida[20];
 	sprintf(fecha_partida, "%04d-%02d-%02d %02d:%02d:%02d", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
 	strcpy(partidas[num_partidas].fecha, fecha_partida);
 	strcpy(partidas[num_partidas].alias, alias);
-	strcpy(partidas[num_partidas].resultado, exploradas == CASILLAS_SIN_MINAS ? "Ganada" : "Perdida");
-	num_partidas++;
+        strcpy(partidas[num_partidas].resultado, 
+		   game_over == 2 ? "Abandonada" : (exploradas == CASILLAS_SIN_MINAS ? "Ganada" : "Perdida"));	num_partidas++;
 	
 	// Actualizar partidas ganadas si el jugador ganó
 	if (exploradas == CASILLAS_SIN_MINAS) {
